42f07ff58da4e58124ffc3ba4fd1eeb687a32de3
dadoudidon <davidthurion@gmail.com>
2019-12-12 13:22:56 +0100

    :construction: Update js and add function

--- js/main.js
+++ js/main.js
@@ -1,48 +1,52 @@
-const resetBtn = document.querySelector("#reset-btn");
-const submitBtn = document.querySelector("#submit-btn");
+const $resetBtn = document.querySelector("#reset-btn");
+const $submitBtn = document.querySelector("#submit-btn");
 
 function checkEmail() {
-  const emailInput = document.querySelector("#email-input");
-  if (emailInput.value.includes("@")) {
-    emailInput.classList.toggle("is-valid");
+  const $emailInput = document.querySelector("#email-input");
+  if ($emailInput.value.includes("@")) {
+    // tester l'email avec une regexp
+    $emailInput.classList.toggle("is-valid");
+    // validField($emailInput);
   } else {
-    emailInput.classList.toggle("is-invalid");
+    $emailInput.classList.toggle("is-invalid");
   }
 }
 
 function checkPassword() {
-  const passWordInput = document.querySelector("#password-input");
-  if (passWordInput.value.length >= 10) {
-    passWordInput.classList.toggle("is-valid");
+  const $passWordInput = document.querySelector("#password-input");
+  if ($passWordInput.value.length >= 10) {
+    // Le mot de passe doit contenir une majuscule, un chiffre
+    $passWordInput.classList.toggle("is-valid");
   } else {
-    passWordInput.classList.toggle("is-invalid");
+    $passWordInput.classList.toggle("is-invalid");
   }
 }
 
-function resetClass() {
-  const formElement = document.querySelectorAll(".form-control");
-  formElement.forEach(element => {
-    element.classList.remove("is-invalid");
-    element.classList.remove("is-valid");
+function resetFormValidation($formElement) {
+  $formElement.forEach($element => {
+    $element.classList.remove("is-invalid");
+    $element.classList.remove("is-valid");
   });
 }
 
-function cleanInput() {
-  const formElement = document.querySelectorAll(".form-control");
-  formElement.forEach(element => {
-    element.value = "";
+function cleanInput($formElement) {
+  $formElement.forEach($element => {
+    $element.value = "";
   });
 }
 
-resetBtn.addEventListener("click", event => {
-  event.preventDefault();
-  resetClass();
-  cleanInput();
+$resetBtn.addEventListener("click", () => {
+  const $formElement = document.querySelectorAll(".form-control");
+  resetFormValidation($formElement);
+  cleanInput($formElement);
 });
 
-submitBtn.addEventListener("click", event => {
+$submitBtn.addEventListener("click", event => {
+  const $formElement = document.querySelectorAll(".form-control");
   event.preventDefault();
-  resetClass();
+  resetFormValidation($formElement);
   checkEmail();
   checkPassword();
+
+  // Envoyer le formulaire si tout est valide via une action ajax SendForm
 });
